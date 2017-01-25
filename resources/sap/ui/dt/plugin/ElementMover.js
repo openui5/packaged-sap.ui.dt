61be1c761b1e015ef873881bcb3ea7d2114115a9
OpenUI5 <openui5@sap.com>
2017-01-25 10:06:20 +0100

    1.38.18

--- resources/sap/ui/dt/plugin/ElementMover.js
+++ resources/sap/ui/dt/plugin/ElementMover.js
@@ -1,6 +1,6 @@
 /*
  * ! UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2016 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2017 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -18,7 +18,7 @@ sap.ui.define([
 	 * @class The ElementMover enables movement of UI5 elements based on aggregation types, which can be used by drag and drop or cut and paste
 	 *        behavior.
 	 * @author SAP SE
-	 * @version 1.38.16
+	 * @version 1.38.18
 	 * @constructor
 	 * @private
 	 * @since 1.34
