d432d92584e7e9af2b9776fff26a107ccb0c0505
OpenUI5 <openui5@sap.com>
2017-12-05 15:43:08 +0100

    1.50.7

--- resources/sap/ui/dt/ControlObserver.js
+++ resources/sap/ui/dt/ControlObserver.js
@@ -24,7 +24,7 @@ function(jQuery, ManagedObjectObserver) {
 	 * @extends sap.ui.dt.ManagedObjectObserver
 	 *
 	 * @author SAP SE
-	 * @version 1.50.6
+	 * @version 1.50.7
 	 *
 	 * @constructor
 	 * @private
--- resources/sap/ui/dt/test/Element.js
+++ resources/sap/ui/dt/test/Element.js
@@ -20,7 +20,7 @@ function(jQuery, ElementUtil, OverlayRegistry) {
 	 * Utility functionality for Element tests
 	 *
 	 * @author SAP SE
-	 * @version 1.50.6
+	 * @version 1.50.7
 	 *
 	 * @private
 	 * @static
