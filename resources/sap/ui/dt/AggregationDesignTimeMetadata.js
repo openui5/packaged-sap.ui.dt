a6f64408a2e890187e529cd78655da444f9b71e1
OpenUI5 <openui5@sap.com>
2017-12-20 13:37:26 +0100

    1.50.8

--- resources/sap/ui/dt/AggregationDesignTimeMetadata.js
+++ resources/sap/ui/dt/AggregationDesignTimeMetadata.js
@@ -24,7 +24,7 @@ function (jQuery, DesignTimeMetadata) {
 	 * @extends sap.ui.core.DesignTimeMetadata
 	 *
 	 * @author SAP SE
-	 * @version 1.50.7
+	 * @version 1.50.8
 	 *
 	 * @constructor
 	 * @private
