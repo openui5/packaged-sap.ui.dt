32ec99ea6b335daa4ed67a5b9f66d370146a4d96
OpenUI5 <openui5@sap.com>
2020-02-03 15:31:19 +0100

    1.52.38

--- resources/sap/ui/dt/DesignTime.js
+++ resources/sap/ui/dt/DesignTime.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2019 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2020 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -31,7 +31,7 @@ function(ManagedObject, ElementOverlay, OverlayRegistry, Selection, ElementDesig
 	 * @extends sap.ui.base.ManagedObject
 	 *
 	 * @author SAP SE
-	 * @version 1.52.37
+	 * @version 1.52.38
 	 *
 	 * @constructor
 	 * @private
