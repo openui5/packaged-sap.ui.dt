56e339d1b53178c64b8c692b594249a6164ed8b3
OpenUI5 <openui5@sap.com>
2020-08-10 21:14:56 +0200

    1.44.46

--- resources/sap/ui/dt/command/SimpleFormMove.js
+++ resources/sap/ui/dt/command/SimpleFormMove.js
@@ -1,6 +1,6 @@
 /*
  * ! UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2019 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2020 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 sap.ui
@@ -15,7 +15,7 @@ sap.ui
 					 * @class
 					 * @extends sap.ui.dt.command.BaseCommand
 					 * @author SAP SE
-					 * @version 1.44.44
+					 * @version 1.44.46
 					 * @constructor
 					 * @private
 					 * @since 1.40
